1175ec6cb01fc88eb0cffd5677787a878426bf44
knighttg1202 <knighttg@miamioh.edu>
2023-11-13 16:47:09 -0500

    Update AppTest.php
    Test cases for future features.

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -14,17 +14,19 @@ Class AppTest extends TestCase {
       $this->assertSame("Environmental", $app->category);
     }
     public function test_clicked_application_returns_correct_info () : void {
-      $response = applications[0];
+      $response = application[0];
       $this->assertSame("EcoExplorer", $response->name);
       $this->assertSame("Environmental" $response->category);
     }
   
     public function test_sort_returns_correct_order(): void {
-      
+      String[] $sortApps = applications.sort(app => app.category.toLowerCase().includes("Entertainment"));
+      $this->assertSame("MindMaze")
     }
   
     public function test_filter_returns_correct_output(): void {
-      
+      String[] $filteredApps = applications.filter(app => app.category.toLowerCase().includes("Fitness"));
+      $this->assertSame("FitFusion", $filteredApps[0].name);
     }
   
     //Test to make sure that when you login as user@gmail.com : password
