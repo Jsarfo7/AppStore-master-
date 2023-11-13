fe0e6d334636a2ef868ed1ce24e308bef2ff25e8
knighttg1202 <knighttg@miamioh.edu>
2023-11-12 21:29:27 -0500

    Update AppTest.php
    Created two test cases. One for clicking the initial application in the list. And one for the application constructor. The application class still needs implemented.

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -8,13 +8,19 @@ use Tests\TestCase;
 
 Class AppTest extends TestCase {
   
+    public function application_constructor_testing () : void {
+      $app = new Application("EcoExplorer", "Environmental", "This is a description", "img link");
+      $this->assertSame("EcoExplorer", $app->name);
+      $this->assertSame("Environmental", $app->category);
+    }
     public function test_clicked_application_returns_correct_info () : void {
-      $response = resources.views."welcome.blade.php".applications[0].name;
-      $response->assertStatus("EcoExplorer");
+      $response = applications[0];
+      $this->assertSame("EcoExplorer", $response->name);
+      $this->assertSame("Environmental" $response->category);
     }
   
     public function test_sort_returns_correct_order(): void {
-    
+      
     }
   
     public function test_filter_returns_correct_output(): void {
@@ -23,6 +29,7 @@ Class AppTest extends TestCase {
   
     //Test to make sure that when you login as user@gmail.com : password
     public function test_login(): void {
+
     }
   
   }
